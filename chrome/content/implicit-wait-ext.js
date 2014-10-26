2df54ccac4105c160bb3c526d4b351616b3c4169
florentbr <florentbr@gmail.com>
2014-10-26 14:20:23 +0000

    Full refactoring

--- chrome/content/implicit-wait-ext.js
+++ chrome/content/implicit-wait-ext.js
@@ -1,47 +1,42 @@
+/*
+ * Copyright 2012 Florent Breheret
+ * http://code.google.com/p/selenium-implicit-wait/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
- * @Author : Florent BREHERET
- * @Function : Activate an implicite wait on action commands when trying to find elements.
- * @Param timeout : Timeout in millisecond, set 0 to disable the implicit wait
- * @Exemple 1 : setImplicitWaitLocator | 0
- * @Exemple 1 : setImplicitWaitLocator | 1000
+ * Specifies the amount of time it should wait when searching for an element if it is not immediately present.
+ * @param {Integer} timeout Timeout in millisecond, set 0 to disable it
+ * @exemple
+     setImplicitWait | 0
+     setImplicitWait | 1000
  */
-Selenium.prototype.doSetImplicitWaitLocator = function(timeout){
-	if( !editor.implicitwait ) throw new SeleniumError("setImplicitWaitLocator works on Selenium IDE only ! ");
-	if( timeout==0 ) {
-		editor.implicitwait.locatorTimeout=0;
-		editor.implicitwait.isImplicitWaitLocatorActivated=false;
-	}else{
-		editor.implicitwait.locatorTimeout=parseInt(timeout);
-		editor.implicitwait.isImplicitWaitLocatorActivated=true;
-	}
+Selenium.prototype.doSetImplicitWait = function(timeout){
+    window.editor.implicitwait.setImplicitWait(timeout);
 };
+
 /**
- 
- 
- * @author : Florent BREHERET
- * @Function : Activate an implicite wait for condition before commands are executed.
- * @Param timeout : Timeout in millisecond, set 0 to disable the implicit wait
- * @Param condition_js : Javascript logical expression that need to be true to execute each command.
- *
- * @Exemple 0 : setImplicitWaitCondition |  0  |  
- * @Exemple 1 : setImplicitWaitCondition |  1000  | (typeof window.Sys=='undefined') ? true : window.Sys.WebForms.PageRequestManager.getInstance().get_isInAsyncPostBack()==false;
- * @Exemple 2 : setImplicitWaitCondition |  1000  | (typeof window.dojo=='undefined') ? true : window.dojo.io.XMLHTTPTransport.inFlight.length==0;
- * @Exemple 3 : setImplicitWaitCondition |  1000  | (typeof window.Ajax=='undefined') ? true : window.Ajax.activeRequestCount==0;
- * @Exemple 4 : setImplicitWaitCondition |  1000  | (typeof window.tapestry=='undefined') ? true : window.tapestry.isServingRequests()==false;
- * @Exemple 4 : setImplicitWaitCondition |  1000  | (typeof window.jQuery=='undefined') ? true : window.jQuery.active==0;
+ * Specifies the amount of time it should wait for a condition to be true to continue to the next command.
+ * @param {Integer} timeout Timeout in millisecond, set 0 to disable it
+ * @param {String} condition_js Javascript logical expression that need to be true to execute each command.
+ * @exemple
+     setImplicitWaitCondition |  0  |  
+     setImplicitWaitCondition |  1000  | !window.Sys || !window.Sys.WebForms.PageRequestManager.getInstance().get_isInAsyncPostBack();
+     setImplicitWaitCondition |  1000  | !window.dojo || !window.dojo.io.XMLHTTPTransport.inFlight.length;
+     setImplicitWaitCondition |  1000  | !window.Ajax || !window.Ajax.activeRequestCount;
+     setImplicitWaitCondition |  1000  | !window.tapestry || !window.tapestry.isServingRequests();
+     setImplicitWaitCondition |  1000  | !window.jQuery || !window.jQuery.active;
  */
-Selenium.prototype.doSetImplicitWaitCondition = function( timeout, condition_js ) {
-	if( !editor.implicitwait ) throw new SeleniumError("setImplicitWaitCondition works on Selenium IDE only ! ");
-	if( timeout==0 ) {
-		editor.implicitwait.conditionTimeout=0;
-		editor.implicitwait.implicitAjaxWait_Condition=null;
-		editor.implicitwait.implicitAjaxWait_Function=function(){return true;};
-		editor.implicitwait.isImplicitWaitAjaxActivated=false;
-	}else{
-		editor.implicitwait.conditionTimeout=parseInt(timeout);
-		editor.implicitwait.implicitAjaxWait_Condition=condition_js;
-		selenium.getEval('editor.implicitwait.implicitAjaxWait_Function=function(){ return eval("' + condition_js + '");};');
-		editor.implicitwait.isImplicitWaitAjaxActivated=true;
-	}
+Selenium.prototype.doSetImplicitWaitCondition = function(timeout, condition_js) {
+    window.editor.implicitwait.setImplicitWaitCondition(timeout, condition_js);
 }
