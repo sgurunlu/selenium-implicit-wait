65a1f12f67348228ae259d951faa20e8529ffbd8
florentbr@gmail.com <florentbr@gmail.com@c210c2b8-d72e-3ce9-01ec-1e9c82fb0bf9>
2014-10-26 14:20:23 +0000

    Full refactoring

--- trunk/chrome/content/implicit-wait-ide.js
+++ trunk/chrome/content/implicit-wait-ide.js
@@ -13,154 +13,183 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-function ImplicitWait(editor) {
-	var self=this;
-	this.editor = editor;
-	this.buttonHouglassChecked = false;
-	this.isImplicitWaitLocatorActivated = false;
-	this.isImplicitWaitAjaxActivated = false;
-	this.implicitAjaxWait_Condition=null;
-	this.implicitAjaxWait_Function=function(){ return true;};
-	this.locatorTimeout=0;
-	this.conditionTimeout=0;
-	this.isLogEnabled=true;
-	editor.app.addObserver({
-		testSuiteChanged: function(testSuite) {
-			if (!self.editor.selDebugger.isHooked) {
-				self.editor.selDebugger.isHooked = self.HookAnObjectMethodAfter(self.editor.selDebugger, 'init', self, self.editor_selDebugger_init_hooked);
-			}
-		}
-	});
-	self.HookAnObjectMethodBefore(Editor.controller, 'doCommand', self, self.Editor_controller_doCommand_hooked);
+
+(function(){
+
+var DEFAULT_TIMEOUT = 5000;
+
+MozillaBrowserBot.prototype.findElement = function (locator, win){
+    var element = base.findElementOrNull(locator, win);
+    if(element === null) 
+        throw new ElementNotFountError(locator);
+    return window.core.firefox.unwrap(element);
+};
+
+/**
+ * Class: Adds the implicit wait feature to SeleniumIDE.
+ * @param {Object} editor
+ */
+function ImplicitWait(editor){
+    this.editor = editor;
+    
+    setTimeout(function(){      //waits all the sub-scripts are loaded to wrap selDebugger.init 
+        wrap(editor.selDebugger, 'init', this, this.wrap_selDebugger_init);
+    }.bind(this), 0);
+}
+ImplicitWait.prototype = {
+    
+    wait_forced: false,
+    wait_timeout: 0,
+    postcondition_timeout: 0,
+    postcondition_func: null,
+    postcondition_run: null,
+    
+    /** Callback for the click on the  hourglass button*/
+    toggleButton: function(button) {
+        if((this.wait_forced = (button.checked ^= true)))
+            this.wait_timeout = DEFAULT_TIMEOUT;
+        else
+            this.wait_timeout = 0;
+    },
+    
+    /** Call from the setImplicitWait command*/
+    setImplicitWait: function(timeout){
+        this.wait_timeout = +timeout || 0;
+    },
+        
+    /** Call from the setImplicitWaitCondition command*/
+    setImplicitWaitCondition: function(timeout, condition_js){
+        if((this.postcondition_timeout = +timeout || 0)){
+            this.postcondition_func = new Function('return ' + condition_js);
+            this.postcondition_func.__string__ = condition_js;
+        }else{
+            this.postcondition_func = null;
+        }
+        this.postcondition_run = null;
+    },
+    
+    /** Overrides Debugger.init: function() in debugger.js line 23 */
+    wrap_selDebugger_init: function(base, fn, args/*[]*/){
+        fn.apply(base, args);   //calls the original method
+        base.runner.MozillaBrowserBot.prototype.findElement = this.new_PageBot_findElement;
+        wrap(base.runner.IDETestLoop.prototype, 'resume', this, this.wrap_IDETestLoop_resume);
+        this.wait_timeout = (this.wait_forced && DEFAULT_TIMEOUT) || 0;
+        this.postcondition_timeout = 0;
+        this.postcondition_func = this.postcondition_run = null;
+    },
+    
+    /** New BrowserBot.prototype.findElement: function(locator, win) in selenium-browserbot.js line 1524 */
+    new_PageBot_findElement: function(locator, win) {
+        var element = this.findElementOrNull(locator, win);
+        if(element === null) 
+            throw new ElementNotFountError(locator);
+        return window.core.firefox.unwrap(element);
+    },
+    
+    /** Overrides TestLoop.prototype.resume: function() in selenium-executionloop.js line 71 */
+    wrap_IDETestLoop_resume: function(base, fn, args/*[]*/){
+        var selDebugger = this.editor.selDebugger,
+            runner = selDebugger.runner,
+            selenium = runner.selenium,
+            browserbot = selenium.browserbot,
+            LOG = runner.LOG;
+        
+        LOG.debug("currentTest.resume() - actually execute modified");
+        browserbot.runScheduledPollers();
+        
+        var command = base.currentCommand;
+        LOG.info("Executing: |" + command.command + " | " + command.target + " | " + command.value + " |");
+
+        var handler = base.commandFactory.getCommandHandler(command.command);
+        if (handler === null)
+            throw new SeleniumError("Unknown command: '" + command.command + "'");
+            
+        command.target = selenium.preprocessParameter(command.target);
+        command.value = selenium.preprocessParameter(command.value);
+        LOG.debug("Command found, going to execute " + command.command);
+        
+        runner.updateStats(command.command);
+        
+        var locator_endtime = this.wait_timeout && new Date().getTime() + this.wait_timeout;
+        var self = this;
+        (function loopFindElement(){
+            try{
+                base.result = handler.execute(selenium, command);
+                base.waitForCondition = base.result.terminationCondition;
+                (function loopCommandCondition(){    //handles the andWait condition in replacement of continueTestWhenConditionIsTrue
+                    try{
+                        browserbot.runScheduledPollers();
+                        if(base.waitForCondition && !base.waitForCondition())
+                            return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopCommandCondition, 15);
+                        base.waitForCondition = null;
+                        var postcondition_endtime = self.postcondition_run && new Date().getTime() + self.postcondition_timeout;
+                        self.postcondition_run = self.postcondition_func;
+                        (function loopPostCondition(){    //handles the customized postcondition
+                            if(postcondition_endtime){
+                                try{
+                                    if(new Date().getTime() > postcondition_endtime)
+                                        base.result = {failed: true, failureMessage: 'Timed out on postcondition ' + self.postcondition_func.__string__};
+                                    else if(!self.postcondition_func.call(selenium))
+                                        return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopPostCondition, 15);
+                                }catch(e){
+                                     base.result = {failed: true, failureMessage: 'Exception on postcondition ' + self.postcondition_func.__string__ + '  Exception:' + extractExceptionMessage(e)};
+                                }
+                            }
+                            base.commandComplete(base.result);
+                            base.continueTest();
+                        })();
+                    }catch(e){
+                        base.result = {failed: true, failureMessage: extractExceptionMessage(e)};
+                        base.commandComplete(base.result);
+                        base.continueTest();
+                    }
+                })();
+            }catch(e){
+                if(e.isElementNotFoundError && locator_endtime && new Date().getTime() < locator_endtime)
+                    return selDebugger.state !== 2/*PAUSE_REQUESTED*/ && window.setTimeout(loopFindElement, 20);
+                if(base._handleCommandError(e))
+                    base.continueTest();
+                else
+                    base.testComplete();
+            }
+        })();
+    }
+};
+
+
+/**
+ * Class: Error specific to findElement
+ * @param {String} locator
+ */
+function ElementNotFountError(locator) {
+    this.locator = locator;
+}
+ElementNotFountError.prototype = {
+    isSeleniumError: true,
+    isElementNotFoundError: true,
+    get message(){ return 'Element ' + this.locator + ' not found'; },
+    toString: function(){ return this.message; }
+};
+
+
+/**
+ * Wraps a method call to a function on a specified context. Skips the wrapping if already done.
+ * @param {Object} obj Object containing the function to intercept
+ * @param {String} key Name of the function to intercept
+ * @param {Object} context Object on which the intercepting function func will be applied
+ * @param {Function} func  Function intercepting obj[method] : function(context, function, arguments)
+ */
+function wrap(obj, key, context, func){
+    var fn = obj[key], w;
+    if(!(w=fn.__wrap__) || w.src !== fn || w.tgt !== func ){
+        (obj[key] = function(){
+            return func.call(context, this, fn, arguments);
+        }).__wrap__ = {src:fn, tgt:func};
+    }
 }
 
-ImplicitWait.prototype.Editor_controller_doCommand_hooked = function( object, arguments ) {
-	switch (arguments[0]) {
-		case "cmd_selenium_play":
-		case "cmd_selenium_play_suite":
-		case "cmd_selenium_reload":
-			this.locatorTimeout=this.editor.getOptions().timeout;
-			this.isImplicitWaitLocatorActivated = this.buttonHouglassChecked;
-			this.isImplicitWaitAjaxActivated = false;
-		break;
-	}
-	return true;
-};
 
-ImplicitWait.prototype.editor_selDebugger_init_hooked = function( object, arguments, retvalue  ) {
-	object.runner.LOG.debug('Implicit wait installation');
-	object.runner.IDETestLoop.prototype.resume = Function_Override_TestLoop_resume;
-	object.runner.PageBot.prototype.findElement = Function_Override_BrowserBot_findElement;
-	this.HookAnObjectMethodBefore(object.runner.LOG, 'log', this, function(){return this.isLogEnabled} );
-};
+//Instantiates the plug-in
+this.editor.implicitwait = new ImplicitWait(this.editor);
 
-ImplicitWait.prototype.toggleImplicitWaitButton = function(button) {
-	button.checked = !button.checked;
-	this.buttonHouglassChecked = button.checked;
-	this.isImplicitWaitLocatorActivated = this.buttonHouglassChecked;
-};
-
-var Function_Override_BrowserBot_findElement = function (locator, win){
-	var element = this.findElementOrNull(locator, win);
-	if (element == null) {
-		throw {
-			isFindElementError: true,
-			message: "Element " + locator + " not found"
-		}
-	}
-	return core.firefox.unwrap(element);
-};
-
-var Function_Override_TestLoop_resume = function() {
-	try {
-		var self=this;
-		if(this.abord) return;
-		if(editor.selDebugger.state == Debugger.PAUSE_REQUESTED){
-			return this.continueTestAtCurrentCommand();
-		}
-		if (editor.implicitwait.isImplicitWaitAjaxActivated && !this.currentCommand.implicitElementWait_EndTime) {
-			if( !this.currentCommand.implicitAjaxWait_EndTime ){
-					this.currentCommand.implicitAjaxWait_EndTime = ( new Date().getTime() + parseInt(editor.implicitwait.conditionTimeout * 0.8) ) ;
-					return window.setTimeout( function(){return self.resume.apply(self);}, 3);
-			}
-			if (new Date().getTime() > this.currentCommand.implicitAjaxWait_EndTime) {
-				throw new SeleniumError("Implicit wait timeout reached while waiting for condition \"" + editor.implicitwait.implicitAjaxWait_Condition  + "\"");
-			}else{
-				try{
-					ret = editor.implicitwait.implicitAjaxWait_Function.call(editor.selDebugger.runner.selenium);
-					//ret=eval(editor.implicitwait.implicitAjaxWait_Condition);
-					//ret=(function(condition){return eval(condition);}).call(editor.selDebugger.runner.selenium, editor.implicitwait.implicitAjaxWait_Condition)
-				} catch (e) {
-					throw new SeleniumError("ImplicitWaitCondition failed : " + e.message );
-				}
-				if(!ret) return window.setTimeout( function(){return self.resume.apply(self);}, 20);
-			}
-		}
-		if(editor.implicitwait.isImplicitWaitLocatorActivated){
-			if(!this.currentCommand.implicitElementWait_EndTime){
-				this.currentCommand.implicitElementWait_EndTime = ( new Date().getTime() + parseInt(editor.implicitwait.locatorTimeout * 0.8) ) ;
-			}
-		}
-		editor.selDebugger.runner.selenium.browserbot.runScheduledPollers();
-		this._executeCurrentCommand();
-		this.continueTestWhenConditionIsTrue();
-	} catch (e) {
-		if(e.isFindElementError){
-			if(editor.implicitwait.isImplicitWaitLocatorActivated){
-				if( new Date().getTime() < this.currentCommand.implicitElementWait_EndTime){
-					editor.implicitwait.isLogEnabled = false;
-					return window.setTimeout( function(){return self.resume.apply(self);}, 20);
-				}else{
-					e = SeleniumError( "Implicit wait timeout reached. " + e.message );
-				}
-			}else{
-				e = SeleniumError( e.message );
-			}
-		}
-		editor.implicitwait.isLogEnabled = true;
-		if(!this._handleCommandError(e)){
-			this.testComplete();
-		}else {
-			this.continueTest();
-		}
-	}
-	editor.implicitwait.isLogEnabled = true;
-};
-
-ImplicitWait.prototype.HookAnObjectMethodBefore = function(ClassObject, ClassMethod, HookClassObject, HookMethod) {
-  if (ClassObject) {
-	var method_id = ClassMethod.toString() + HookMethod.toString();
-    if (!ClassObject[method_id]) {
-	  ClassObject[method_id] = ClassObject[ClassMethod];
-	  ClassObject[ClassMethod] = function() {
-		if( HookMethod.call(HookClassObject, ClassObject, arguments )==true ){
-			return ClassObject[method_id].apply(ClassObject, arguments);
-		}
-      }
-	  return true;
-    }
-  }
-  return false;
-};
-
-ImplicitWait.prototype.HookAnObjectMethodAfter = function(ClassObject, ClassMethod, HookClassObject, HookMethod) {
-  if (ClassObject) {
-	var method_id = ClassMethod.toString() + HookMethod.toString();
-    if (!ClassObject[method_id]) {
-	  ClassObject[method_id] = ClassObject[ClassMethod];
-	  ClassObject[ClassMethod] = function() {
-        var retvalue = ClassObject[method_id].apply(ClassObject, arguments);
-        return HookMethod.call(HookClassObject, ClassObject, arguments, retvalue );
-      }
-	  return true;
-    }
-  }
-  return false;
-};
-
-try {
-	this.editor.implicitwait = new ImplicitWait(this.editor);
-} catch (error) {
-	alert('Error in ImplicitWait: ' + error);
-}
+})();
